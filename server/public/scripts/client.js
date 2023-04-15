f5d523f9c570ab3fcbaf4d81888d7fc5a577dbbf
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-04-14 20:50:16 -0500

    confirmed getting inside of RenderTaskList

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,13 @@ $(document).ready(onReady)
 
 function onReady() {
   console.log('JS & JQ sourced in');
-}
+
+  // Get Task list and update DOM
+  getAndRenderTaskList();
+}// End onReady
+
+
+function getAndRenderTaskList() {
+  console.log('inside of getAndRenderTaskList');
+}// End getAndRenderTaskList
+

6b0342fd533a1c1638c219c67dc152de61e68c19
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-04-14 21:06:37 -0500

    Added mark complete and delete task btn
    -index.html
    	-added mark complete and delete task buttons to the task list table
    
    -client.js
    	-built logic to check if task has been completed or not.
    	-if task is complete apply class of completedTask when appending to DOM
    	  and leave off completeTaskBtn from row
    	-if task is not complete, add compelteTaskBtn to the row.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -10,5 +10,58 @@ function onReady() {
 
 function getAndRenderTaskList() {
   console.log('inside of getAndRenderTaskList');
+
+  // Reach out to server asking for the array of
+  // task objects:
+  $.ajax({
+    method: 'GET',
+    url: '/toDo'
+  }).then(function(response) {
+    console.log('inside of then for GET /toDo');
+
+    // Clear out the viewToDoList table body
+    $('#viewToDoList').empty();
+
+    // Loop through the array and render each task
+    //  onto the DOM
+    for (let task of response) {
+      if (task.completed === true) {
+        $('#viewToDoList').append(`
+          <tr data-id=${task.id} class="completedTask">
+            <td>${task.task}</td>
+            <td>${task.task_note}</td>
+            <td>${task.assigned_to}</td>
+            <td>${task.created}</td>
+            <td><!-- This space intentionally left blank --></td>
+            <td>${task.completed_date}</td>
+            <td>
+              <button class="deleteTaskBtn">Delete Task</button>
+            </td>
+          </tr>
+        `)
+      }
+      else {
+        $('#viewToDoList').append(`
+          <tr data-id=${task.id}>
+            <td>${task.task}</td>
+            <td>${task.task_note}</td>
+            <td>${task.assigned_to}</td>
+            <td>${task.created}</td>
+            <td>
+              <button class="completeTaskBtn">Complete Task</button>
+            </td>
+            <td><!-- This space intentionally left blank --></td>
+            <td>
+              <button class="deleteTaskBtn">Delete Task</button>
+            </td>
+          </tr>
+        `)
+      }
+    }
+
+  }).catch(function(error) {
+    alert('There was an error in getting the current task list')
+    console.log(error);
+  })
 }// End getAndRenderTaskList
 

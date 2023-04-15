4e51ff6918b8ce879c164bd730a477a65a14a5d9
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-04-14 21:24:28 -0500

    Built out GET for /toDo
    -pool.query set up to get all data from to_do_list sorted by date task created
    -Updated pool.js to remove timestamp from date when pulled from table
    
    -Need to adjust how green color is applied when task is completed.
    	-Current .css styling is being overriden by white/gray alternating lines

--- server/routes/toDoList.router.js
+++ server/routes/toDoList.router.js
@@ -7,6 +7,26 @@ const pool = require('../modules/pool.js');
 
 
 // GET
+toDoListRouter.get('/', (req, res) => {
+  console.log('inside of GET /toDo');
+
+  let sqlText = `
+    SELECT * FROM "to_do_list"
+    ORDER BY "created";
+  `;
+
+  pool.query(sqlText)
+    .then((dbRes) => {
+      let taskList = dbRes.rows;
+      res.send(taskList);
+    })
+    .catch((dbErr) => {
+      // Log that there was an issue inside of this function
+      console.log('SQL query in GET /toDo failed:', dbErr);
+      // Send "Internal Server Error" status to client
+      res.sendStatus(500);
+    })
+})
 
 
 // POST
